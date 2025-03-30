8efd05fa07a071bc077727aed46021fdda5fd597
Jason-Lee3 <Jasoneugenelee@gmail.com>
2025-03-29 22:18:20 -0700

    Method Overloading

--- src/src/MethodOverloading.java
+++ src/src/MethodOverloading.java
@@ -1,3 +1,14 @@
 public class MethodOverloading {
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
 
+    public static double convertToCentimeters(int feet, int inches) {
+        return convertToCentimeters((feet * 12) + inches);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
+        System.out.println("68in = " + convertToCentimeters(68) + " cm");
+    }
 }
