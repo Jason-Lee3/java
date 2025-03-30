3dbeedc26c975dcfaf5f8ac95a29daa467e45f88
Jason-Lee3 <Jasoneugenelee@gmail.com>
2025-03-29 22:46:44 -0700

    Exceptions with try-resource

---
+++ src/src/JavaExceptions/Main.java
@@ -0,0 +1,56 @@
+package JavaExceptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String filename = "testing.csv";
+        testFile2(null);
+        File file = new File(filename);
+        if (!file.exists()) {
+            System.out.println("I can't run unless this file exists");
+            return;
+        }
+        System.out.println("good to go");
+    }
+
+    private static void testFile(String filename) {
+        Path path = Paths.get(filename);
+        FileReader reader = null;
+        try {
+            // List<String> lines = Files.readAllLines(path);
+            reader = new FileReader(filename);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("Maybe I'd log something either way...");
+        }
+        System.out.println("File exists and able to use as a resource");
+    }
+
+    private static void testFile2(String filename) {
+        try (FileReader reader = new FileReader(filename)) {
+        } catch (NullPointerException | IllegalArgumentException badData) {
+            System.out.println("User has added bad data " + badData.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println("Something unrelated and unexpected");
+        }
+        System.out.println("File exists and able to use as a resource.");
+    }
+}
