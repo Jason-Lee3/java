3195f770a133012b14b92685b0cff15c7e780c7f
Jason-Lee3 <Jasoneugenelee@gmail.com>
2025-03-30 12:03:42 -0700

    Threads

---
+++ src/src/JavaThreads/ThreadsSleepAndInterrupt.java
@@ -0,0 +1,63 @@
+package JavaThreads;
+
+public class ThreadsSleepAndInterrupt {
+    public static void main(String[] args) {
+        System.out.println("Main thread running");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread thread = new Thread(() -> {
+            String tname = Thread.currentThread().getName();
+            System.out.println(tname + " should take 10 dots to run.");
+            for (int i = 0; i < 10; i++) {
+                System.out.print(". ");
+                try {
+                    Thread.sleep(500);
+                    System.out.println("A. State = " + Thread.currentThread().getState());
+
+                } catch (InterruptedException e) {
+                    System.out.println("\nWhoops!! " + tname + " interrupted.");
+                    System.out.println("A1. State = " + Thread.currentThread().getState());
+                    return;
+                }
+            }
+            System.out.println("\n" + tname + " completed.");
+        });
+
+        Thread installThread = new Thread(() -> {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    Thread.sleep(250);
+                    System.out.println("Installation Step " + ( i + 1 ) + " is completed.");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "InstallThread");
+
+        thread.start();
+        System.out.println("Main thread would continue here");
+
+        long now = System.currentTimeMillis();
+        while (thread.isAlive()) {
+            System.out.println("\nwaiting for thread to complete");
+            try {
+                Thread.sleep(1000);
+                if (System.currentTimeMillis() - now > 2000) {
+                    thread.interrupt();
+                }
+            } catch ( InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if )
+    }
+
+}
