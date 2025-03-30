4ba25c77f60a5efb665d7ccc485e518d9b63a39a
Jason-Lee3 <Jasoneugenelee@gmail.com>
2025-03-29 22:09:25 -0700

    add Main

---
+++ src/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+
+        calculateScore(75);
+        calculateScore(100, 100);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    // overloading
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored" + " scored " + score + " points");
+        return score * 1000;
+    }
+}
